a2f69e16399c83afccc390c4623dec9ed4109dde
Snapper Leonardo <sleonard@oa-k6.21-school.ru>
2019-07-31 19:44:50 +0300

    fixed item breaking wall bug, refactored check_condition with item's id i

--- sources/render/items.c
+++ sources/render/items.c
@@ -12,19 +12,21 @@ void 	draw_items(t_wolf *wolf)
 	}
 }
 
-void	check_condition(t_wolf *wolf, double atan_item, double *cord)
+void check_condition(t_wolf *wolf, double atan_item, double *cord, int i)
 {
 	double	dist;
 
 	dist = sqrt(cord[0] * cord[0] + cord[1] * cord[1]) * 0.25;
 	if (dist < 0.1)
 	{
-		wolf->map.int_map[wolf->map.items[0].y][wolf->map.items[0].x] = 0;
-		wolf->map.items[0].x = 0;
+		wolf->map.int_map[wolf->map.items[i].y][wolf->map.items[i].x] = 0;
+		wolf->map.items[i].x = 0;
 	}
 	// dist = 1;
 	scaled_draw(wolf->sdl, *wolf->textures.weapons[WEAPONS_NUM - 1].sprite,
-				0.75 / dist, (t_point) {(atan_item - wolf->player.angle + wolf->player.fov * 0.5) * 180 / M_PI * 22 - (*wolf->textures.weapons[WEAPONS_NUM - 1].sprite).width * 0.75 / dist * 0.5, WIN_HEIGHT * 0.55 - dist * 4});
+				0.75 / dist, (t_point)
+				{(atan_item - wolf->player.angle + wolf->player.fov * 0.5) * 180 / M_PI * 22 -
+				(*wolf->textures.weapons[WEAPONS_NUM - 1].sprite).width * 0.75 / dist * 0.5, WIN_HEIGHT * 0.55 - dist * 4});
 }
 
 void	draw_item(t_wolf *wolf, int i)
@@ -50,19 +52,19 @@ void	draw_item(t_wolf *wolf, int i)
 		wolf->player.angle -= M_PI * 2;
 	if (atan_item <= wolf->player.angle + wolf->player.fov / 2 &&
 		atan_item >= wolf->player.angle - wolf->player.fov / 2)
-		check_condition(wolf, atan_item, cord);
+		check_condition(wolf, atan_item, cord, i);
 	else if (wolf->player.angle + wolf->player.fov * 0.5 > M_PI * 2)
 	{
 		wolf->player.angle -= 2 * M_PI;
 		if (atan_item >= wolf->player.angle - wolf->player.fov * 0.5 &&
 			atan_item <= wolf->player.angle + wolf->player.fov * 0.5)
-			check_condition(wolf, atan_item, cord);
+			check_condition(wolf, atan_item, cord, i);
 	}
 	else if (atan_item + wolf->player.fov * 0.5 > 2 * M_PI)
 	{
 		atan_item -= 2 * M_PI;
 		if (atan_item >= wolf->player.angle - wolf->player.fov * 0.5 &&
 			atan_item <= wolf->player.angle + wolf->player.fov * 0.5)
-			check_condition(wolf, atan_item, cord);
+			check_condition(wolf, atan_item, cord, i);
 	}
 }
